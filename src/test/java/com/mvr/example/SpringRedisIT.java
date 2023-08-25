51135e5b45cc77d95ecf00c939653a4792827c1b
Mark Ramsey <mramsey2624@gmail.com>
2023-08-25 14:13:18 -0400

    🚧 chore(SpringRedisIT.java): add missing import for MediaType from Spring Framework 🔥 chore(SpringRedisIT.java): remove unnecessary semicolon from throw statement

--- src/test/java/com/mvr/example/SpringRedisIT.java
+++ src/test/java/com/mvr/example/SpringRedisIT.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest(
@@ -30,6 +31,6 @@ public class SpringRedisIT {
 
     @Test void whenGET_Specific_Person_thenStatus200()
         throws Exception {
-          throw new Exception ("Not Implemented")
+          throw new Exception ("Not Implemented");
         }
 }
