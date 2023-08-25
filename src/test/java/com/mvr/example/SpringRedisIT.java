fb750c9d5fe5274f6033d19bd165b4bb473982fe
Mark Ramsey <mramsey2624@gmail.com>
2023-08-25 14:12:45 -0400

    ✅ test(SpringRedisIT.java): add integration test for GET request to ensure endpoint returns expected status code 🔧 chore(SpringRedisIT.java): import necessary static methods for building GET request and asserting status code

--- src/test/java/com/mvr/example/SpringRedisIT.java
+++ src/test/java/com/mvr/example/SpringRedisIT.java
@@ -1,6 +1,9 @@
 
 package com.mvr.example;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
